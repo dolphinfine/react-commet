ef5f7c9bd042b57fd40f458489be28a26b0063b0
dolphinfine <coolfzk@foxmail.com>
2017-08-30 17:34:50 +0800

    提交
    提交

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CommentApp from './CommentApp'
+import './index.css'
+
+ReactDOM.render(
+  <CommentApp />,
+  document.getElementById('root')
+)
